eb9f8c6acf70a8d47b1ae0c8075fdee998baeb9c
asdfghjrussel <asdfghjrussel@gmail.com>
2020-04-27 21:13:29 +0800

    adds a python script containing basic mathematical operations

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+def add(first_num, second_num):
+    return first_num + second_num
+def subtract(first_num, second_num):
+    return first_num - second_num
+def multiply(first_num, second_num):
+    return first_num * second_num
+def divide(first_num, second_num):
+    return first_num / second_num
+
+if __name__ == '__main__':
+        print(add(10, 2))
+        print(subtract(10,2))
+        print(multiply(10,2))
+        print(divide(10,2))
